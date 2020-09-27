f00cbfcc19d7c7eb3808ee0abcc572eb5c31e3ee
GengHaoChen <GengHaoChenLove@Gmail.com>
2020-09-27 22:53:03 +0800

    init

---
+++ student-score-manage/subject-repository/src/main/java/org/graduate/subject/repository/model/Subject.java
@@ -0,0 +1,31 @@
+package org.graduate.subject.repository.model;
+
+public class Subject {
+    private Long id;
+    private String no;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ student-score-manage/teacher-repository/src/main/java/org/graduate/teacher/repository/model/Teacher.java
@@ -0,0 +1,87 @@
+package org.graduate.teacher.repository.model;
+
+import java.util.Date;
+
+public class Teacher {
+    private Long id;
+    private String no;
+    private String name;
+    private String password;
+    private String gender;
+    private String telephone;
+    private Date admissionDate;
+    private Date resignDate;
+    private Integer status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public Date getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(Date admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public Date getResignDate() {
+        return resignDate;
+    }
+
+    public void setResignDate(Date resignDate) {
+        this.resignDate = resignDate;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
