37223f0f92ac95a51b399aef457f435fc6af6367
GengHaoChen <GengHaoChenLove@Gmail.com>
2020-09-29 00:31:58 +0800

    init

---
+++ student-score-manage/student-repository/src/main/java/org/graduate/student/repository/dao/ExamScoreDao.java
@@ -0,0 +1,8 @@
+package org.graduate.student.repository.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExamScoreDao {
+    void save();
+}
--- student-score-manage/student-score-manage-application/src/main/java/org/graduate/student/score/manage/Application.java
+++ student-score-manage/student-score-manage-application/src/main/java/org/graduate/student/score/manage/Application.java
@@ -1,13 +1,14 @@
 package org.graduate.student.score.manage;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.graduate")
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
     }
 }
