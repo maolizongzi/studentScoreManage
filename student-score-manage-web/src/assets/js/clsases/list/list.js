b6d84968afa390a7c020262bfca724f7cdeb61db
GengHaoChen <GengHaoChenLove@Gmail.com>
2020-10-12 21:20:17 +0800

    修复部分bug

--- student-score-manage-web/src/assets/js/clsases/list/list.js
+++ student-score-manage-web/src/assets/js/clsases/list/list.js
@@ -2,40 +2,40 @@
     'use strict';
     var serverUrl = '';
 
-    $.load('/config/config.json', '', function (data) {
-        debugger;
-        console.log(data);
-    });
+    $.when($.getJSON('/config/config.json', function (result) {
+        serverUrl = result['server-url'];
+    })).done(function () {
 
-    $.ajax({
-        url: serverUrl + '/classes/query',
-        type: 'get',
-        async: false,
-        contentType: 'application/json',
-        data: { 'pageSize': 10, 'currentPage': 1 },
-        success: function (result) {
-            debugger;
-            var code = result['code'];
-            var data = result['data'];
-            if (code === '00') {
-                $.each(data, function (index, obj) {
-                    const classesNoTd = $('<td></td>');
-                    classesNoTd.text(obj['no']);
-                    const classesNameTd = $('<td></td>');
-                    classesNameTd.text(obj['name']);
-                    const classesGradeTd = $('<td></td>');
-                    classesGradeTd.text(obj['grade']);
+        $.ajax({
+            url: serverUrl + '/classes/query',
+            type: 'get',
+            async: false,
+            contentType: 'application/json',
+            data: { 'pageSize': 10, 'currentPage': 1 },
+            success: function (result) {
+                debugger;
+                var code = result['code'];
+                var data = result['data'];
+                if (code === '00') {
+                    $.each(data, function (index, obj) {
+                        const classesNoTd = $('<td></td>');
+                        classesNoTd.text(obj['no']);
+                        const classesNameTd = $('<td></td>');
+                        classesNameTd.text(obj['name']);
+                        const classesGradeTd = $('<td></td>');
+                        classesGradeTd.text(obj['grade']);
 
-                    const tr = $('<tr></tr>');
-                    tr.append(classesNoTd);
-                    tr.append(classesNameTd);
-                    tr.append(classesGradeTd);
-                    $('#table-content').append(tr);
-                });
+                        const tr = $('<tr></tr>');
+                        tr.append(classesNoTd);
+                        tr.append(classesNameTd);
+                        tr.append(classesGradeTd);
+                        $('#table-content').append(tr);
+                    });
+                }
+            },
+            error: function (err) {
+                console.log(err);
             }
-        },
-        error: function (err) {
-            console.log(err);
-        }
+        });
     });
 }());
--- student-score-manage-web/src/assets/js/subject/list/list.js
+++ student-score-manage-web/src/assets/js/subject/list/list.js
@@ -1,38 +1,37 @@
 (function () {
     'use strict';
     var serverUrl = '';
+    
+    $.when($.getJSON('/config/config.json', function (result) {
+        serverUrl = result['server-url'];
+    })).done(function () {
+        $.ajax({
+            url: serverUrl + '/subject/query',
+            type: 'get',
+            async: false,
+            contentType: 'application/json',
+            data: { 'pageSize': 10, 'currentPage': 1 },
+            success: function (result) {
+                debugger;
+                var code = result['code'];
+                var data = result['data'];
+                if (code === '00') {
+                    $.each(data, function (index, obj) {
+                        const subjectNoTd = $("<td></td>");
+                        subjectNoTd.text(obj['no']);
+                        const subjectNameTd = $("<td></td>");
+                        subjectNameTd.text(obj['name']);
 
-    $.load('/config/config.json', '', function (data) {
-        debugger;
-        console.log(data);
-    });
-
-    $.ajax({
-        url: serverUrl + '/subject/query',
-        type: 'get',
-        async: false,
-        contentType: 'application/json',
-        data: { 'pageSize': 10, 'currentPage': 1 },
-        success: function (result) {
-            debugger;
-            var code = result['code'];
-            var data = result['data'];
-            if (code === '00') {
-                $.each(data, function (index, obj) {
-                    const subjectNoTd = $("<td></td>");
-                    subjectNoTd.text(obj['no']);
-                    const subjectNameTd = $("<td></td>");
-                    subjectNameTd.text(obj['name']);
-
-                    const tr = $("<tr></tr>");
-                    tr.append(subjectNoTd);
-                    tr.append(subjectNameTd);
-                    $("#table-content").append(tr);
-                });
+                        const tr = $("<tr></tr>");
+                        tr.append(subjectNoTd);
+                        tr.append(subjectNameTd);
+                        $("#table-content").append(tr);
+                    });
+                }
+            },
+            error: function (err) {
+                console.log(err);
             }
-        },
-        error: function (err) {
-            console.log(err);
-        }
+        });
     });
 }());
