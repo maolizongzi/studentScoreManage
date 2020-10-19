827a00736719cb1c4b524d124b78ff9340370cf1
GengHaoChen <GengHaoChenLove@Gmail.com>
2020-10-19 15:43:16 +0800

    教师管理bug修复

--- student-score-manage-web/src/assets/js/teacher/list/list.js
+++ student-score-manage-web/src/assets/js/teacher/list/list.js
@@ -5,20 +5,20 @@
     })).done(function () {
         $('#admission-date').datepicker({ language: 'zh-CN' });
         $('#resign-date').datepicker({ language: 'zh-CN' });
-        build_teacher_table(server_url);
+        build_teacher_table(server_url, 1);
         $('#register-teacher').on('click', function () { to_register_taecher(); });
         $('#cancel-teacher').on('click', function () { clear_teacher(); });
         $('#save-teacher').on('click', function () { save_teacher(server_url) });
     });
 }());
 
-function build_teacher_table(server_url) {
+function build_teacher_table(server_url, current_page) {
     $('#table-content').html('');
     $.ajax({
         url: server_url + '/teacher/query',
         type: 'get',
         contentType: 'application/json',
-        data: { 'pageSize': 10, 'currentPage': 1 },
+        data: { 'pageSize': 10, 'currentPage': current_page },
         success: function (result) {
             let code = result['code'];
             let data = result['data'];
@@ -29,17 +29,13 @@ function build_teacher_table(server_url) {
                     let teacher_name_td = $('<td></td>');
                     teacher_name_td.text(obj['name']);
                     let teacher_gender_td = $('<td></td>');
-                    teacher_gender_td.text(obj['gender']);
+                    teacher_gender_td.text(obj['gender'] === 1 ? '男' : '女');
                     let teacher_telephone_td = $('<td></td>');
                     teacher_telephone_td.text(obj['telephone']);
                     let teacher_admission_date_td = $('<td></td>');
                     teacher_admission_date_td.text(obj['admissionDate']);
                     let teacher_status_td = $('<td></td>');
-                    let teacher_status = '在职';
-                    if (obj['status'] === 1) {
-                        teacher_status = '离职';
-                    }
-                    teacher_status_td.text(teacher_status);
+                    teacher_status_td.text(obj['status'] === 0 ? '在职' : '离职');
                     let option_td = $('<td></td>');
                     let edit_teacher_btn = $('<button>编辑</button>');
                     edit_teacher_btn.attr({ 'class': 'btn btn-outline-primary btn-sm' })
@@ -56,7 +52,7 @@ function build_teacher_table(server_url) {
                     tr.append(option_td);
                     $('#table-content').append(tr);
                 });
-                build_teacher_table_pagination(1, result['totalPage']);
+                build_teacher_table_pagination(current_page, result['totalPage'], server_url);
             }
         },
         error: function (err) {
@@ -65,65 +61,44 @@ function build_teacher_table(server_url) {
     });
 }
 
-function build_teacher_table_pagination(currentPage, totalPage) {
+function build_teacher_table_pagination(current_page, total_page, server_url) {
+    $('#teacher-pagination').html('');
+    let page_interval = 2;
+    let start_index = current_page - page_interval;
+    if (start_index < 1) {
+        start_index = 1;
+        page_interval = page_interval + Math.abs(current_page - 1 - page_interval);
+    }
+
+    let end_index = current_page + page_interval > total_page ? total_page : current_page + page_interval;
+
+
+
+    for (let i = start_index; i <= end_index; i++) {
+        let page_link = $('<a class=\'page-link\'></a>');
+        page_link.text(i);
+        page_link.on('click', function () { build_teacher_table(server_url, i); });
+        let page_li = $('<li class=\'page-item\'></li>');
+        page_li.append(page_link);
+        $('#teacher-pagination').append(page_li);
+    }
+
+
+
     let previous_link = $('<a class=\'page-link\' aria-label=\'Previous\'><span aria-hidden=\'true\'>&laquo;</span></a>');
-    previous_link.on('click', function () { });
+    previous_link.on('click', function () { build_teacher_table(server_url, current_page - 1 < 1 ? 1 : current_page - 1) });
     let previous_li = $('<li class=\'page-item\'></li>');
     previous_li.append(previous_link);
-    $('#teacher-pagination').append(previous_li);
-
-    // if (currentPage === 1) {
-    //     for (let i = 0; i < 5; i++) {
-    //         let page_link = $('<a class=\'page-link\'></a>');
-    //         page_link.text(i + 1);
-    //         page_link.on('click', function () { })
-    //         let page_li = $('<li class=\'page-item\'></li>');
-    //         page_li.append(page_link);
-    //         $('#teacher-pagination').append(page_li);
-    //     }
-    // } else if (currentPage === totalPage) {
-    // } else {
-    // }
-
-
-
-    for (let i = 2; i >= 0; i--) {
-        let page_link = $('<a class=\'page-link\'></a>');
-        page_link.text(currentPage - 1);
-        page_link.on('click', function () { })
-        let page_li = $('<li class=\'page-item\'></li>');
-        page_li.append(page_link);
-        $('#teacher-pagination').append(page_li);
-    }
-
-    let page_link = $('<a class=\'page-link\'></a>');
-    page_link.text(currentPage);
-    page_link.on('click', function () { })
-    let page_li = $('<li class=\'page-item\'></li>');
-    page_li.append(page_link);
-    $('#teacher-pagination').append(page_li);
-
-    for (let i = 0; i > 2; i++) {
-        let page_link = $('<a class=\'page-link\'></a>');
-        page_link.text(currentPage + 1 + i);
-        page_link.on('click', function () { })
-        let page_li = $('<li class=\'page-item\'></li>');
-        page_li.append(page_link);
-        $('#teacher-pagination').append(page_li);
-    }
-
-
-
+    $('#teacher-pagination').prepend(previous_li);
     let next_link = $('<a class=\'page-link\' aria-label=\'Next\'><span aria-hidden=\'true\'>&raquo;</span></a>');
-    next_link.on('click', function () { })
+    next_link.on('click', function () { build_teacher_table(server_url, current_page + 1 > total_page ? total_page : current_page + 1) });
     let next_li = $('<li class=\'page-item\'></li>');
     next_li.append(next_link);
     $('#teacher-pagination').append(next_li);
 }
 
-
 function save_teacher(server_url) {
-    console.log('save_teacher')
+    console.log('save_teacher');
     let input_arr = $('#teacher-data input');
     let data = {}
     $.each(input_arr, function (index, obj) {
@@ -137,13 +112,12 @@ function save_teacher(server_url) {
         }
         data[input_name] = inputVal;
     });
-    let gender = $('#gender').val()
-    let status = $('#status').val()
+    let gender = $('#gender').val();
+    let status = $('#status').val();
     data['gender'] = gender;
     data['status'] = status;
     console.log(data);
-    let action = $('action').val();
-
+    debugger;
     let api_url = server_url + '/teacher/register';
     if ($('#action').val() === 'update') {
         api_url = server_url + '/teacher/update';
@@ -160,8 +134,8 @@ function save_teacher(server_url) {
             let data = result['data'];
             if (code === '00') {
                 $.bootstrapGrowl('保存成功', { type: 'success' });
-                $('#eidt-teacher-modal').modal({ 'show': false });
-                build_teacher_table(server_url);
+                $('#edit-teacher-modal').modal({ 'show': false });
+                build_teacher_table(server_url, 1);
 
             }
         },
@@ -177,13 +151,14 @@ function to_edit_teacher(server_url, teacher_no) {
         url: server_url + '/teacher/query',
         type: 'get',
         contentType: 'application/json',
-        data: { 'no': teacher_no },
+        data: { 'no': teacher_no, 'pageSize': 1, 'currentPage': 1 },
         success: function (result) {
             let code = result['code'];
             let data = result['data'];
-            if (code === '00') {
+            if (code === "00") {
                 let teacher = data[0];
                 $('#id').val(teacher['id']);
+                $('#no').val(teacher['no']);
                 $('#name').val(teacher['name']);
                 $('#admission-date').val(teacher['admissionDate']);
                 $('#resign-date').val(teacher['resignDate']);
@@ -191,7 +166,7 @@ function to_edit_teacher(server_url, teacher_no) {
                 $('#gender').val(teacher['gender']);
                 $('#status').val(teacher['status']);
                 $('#action').val('update');
-                $('#eidt-teacher-modal').modal({ 'show': true });
+                $('#edit-teacher-modal').modal({ 'show': true });
             }
         }
     });
